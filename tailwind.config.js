53dbce4323cca70da494ef9e102b8265bcad8a3c
Francis james <ibolokefrancisjames2@gmail.com>
2024-11-27 12:40:52 +0100

    depressed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ['./build/*.html', './js/*.js'],
   theme: {
     extend: {
       keyframes: {
